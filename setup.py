0c1ae6037675c00954ebd31ca62b6aaa0543ab99
amkrtychian <artur.mkr@gmail.com>
2023-08-03 13:34:18 +0300

    demo

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='My calculator package',
+    install_requires=[
+        # Add your project's dependencies here
+    ],
+)
