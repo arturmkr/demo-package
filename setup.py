f3e2998d839d1419934aeb4202539e4df9b344ba
amkrtychian <artur.mkr@gmail.com>
2024-06-22 14:55:23 +0300

    refactoring

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('metrics/requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='metrics',
     version='0.0.1',
@@ -8,5 +11,6 @@ setup(
     url='',
     description='metrics info',
     packages=find_packages(),
+    install_requires=requirements,
     python_requires='>=3.6',
 )
